9a3a7f7788e8d30ef5c6a4c9d0e4d7a154a92519
Rodrigo <rodrigosilva.emcode@gmail.com>
2022-12-19 08:56:54 -0300

    added framework bootstrap

--- mirrorfashion/checkout.php
+++ mirrorfashion/checkout.php
@@ -5,27 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout Mirror Fashion</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-    
-    <h1>Ótima escolha!</h1>
-    <p>Obrigado por comprar na Mirror Fashion!
-    Preencha seus dados para efetivar a compra.
-    </p>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Ótima escolha!</h1>
+            <p>Obrigado por comprar na Mirror Fashion!
+            Preencha seus dados para efetivar a compra.
+            </p>
 
-    <h2>Sua compra</h2>
-    <dl>
-        <dt>Cor</dt>
-        <dd><?= $_POST['cor'] ?></dd>
+            <h2>Sua compra</h2>
+            <dl>
+                <dt>Cor</dt>
+                <dd><?= $_POST['cor'] ?></dd>
 
-        <dt>Tamanho</dt>
-        <dd><?= $_POST['tamanho'] ?></dd>
+                <dt>Tamanho</dt>
+                <dd><?= $_POST['tamanho'] ?></dd>
 
-        <dt>Produto</dt>
-        <dd><?= $_POST['nome'] ?></dd>
+                <dt>Produto</dt>
+                <dd><?= $_POST['nome'] ?></dd>
 
-        <dt>Preço</dt>
-        <dd><?= $_POST['preco'] ?></dd>
-    </dl>
+                <dt>Preço</dt>
+                <dd><?= $_POST['preco'] ?></dd>
+            </dl>
+        </div>
+    </div>
 </body>
 </html>
