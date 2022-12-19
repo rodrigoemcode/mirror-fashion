5f9545db12f1f6f59ae967c647cb014fa7d65923
Rodrigo <rodrigosilva.emcode@gmail.com>
2022-12-19 10:16:26 -0300

    update checkou.php

--- mirrorfashion/checkout.php
+++ mirrorfashion/checkout.php
@@ -8,27 +8,37 @@
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-    <div class="jumbotron">
-        <div class="container">
-            <h1>Ótima escolha!</h1>
-            <p>Obrigado por comprar na Mirror Fashion!
-            Preencha seus dados para efetivar a compra.
-            </p>
+    <div class="container">
+        <div class="jumbotron">
+            <div class="container">
+                <h1>Ótima escolha!</h1>
+                <p>Obrigado por comprar na Mirror Fashion!
+                Preencha seus dados para efetivar a compra.
+                </p>
 
-            <h2>Sua compra</h2>
-            <dl>
-                <dt>Cor</dt>
-                <dd><?= $_POST['cor'] ?></dd>
+                <div class="panel panel-success">
+                    <div class="panel-heading">
+                        <h2 class="panel-title">Sua compra</h2>
+                    </div>
+                    <div class="panel-body">
+                        <img src="/mirrorfashion/img/produtos/foto1-<?= $_POST["cor"] ?>.png"
+                            class="img-thumbnail img-responsive">
+                        <dl>
+                            <dt>Cor</dt>
+                            <dd><?= $_POST['cor'] ?></dd>
 
-                <dt>Tamanho</dt>
-                <dd><?= $_POST['tamanho'] ?></dd>
+                            <dt>Tamanho</dt>
+                            <dd><?= $_POST['tamanho'] ?></dd>
 
-                <dt>Produto</dt>
-                <dd><?= $_POST['nome'] ?></dd>
+                            <dt>Produto</dt>
+                            <dd><?= $_POST['nome'] ?></dd>
 
-                <dt>Preço</dt>
-                <dd><?= $_POST['preco'] ?></dd>
-            </dl>
+                            <dt>Preço</dt>
+                            <dd><?= $_POST['preco'] ?></dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
