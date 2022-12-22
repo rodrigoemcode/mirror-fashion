42114857a817dcc5e02fd38c5550e5bb4869fac6
Rodrigo <rodrigosilva.emcode@gmail.com>
2022-12-22 09:14:35 -0300

    update checkout.php

--- mirrorfashion/checkout.php
+++ mirrorfashion/checkout.php
@@ -5,104 +5,130 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout Mirror Fashion</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-flatly.css">
     <link rel="stylesheet" href="css/checkout.css">
     </head>
 <body>
     <div class="container">
-        <div class="jumbotron">
-            <div class="container">
-                <h1>Ótima escolha!</h1>
-                <p>Obrigado por comprar na Mirror Fashion!
-                Preencha seus dados para efetivar a compra.
-                </p>
+        <div class="row">
 
-                <div class="panel panel-success">
-                    <div class="panel-heading">
-                        <h2 class="panel-title">Sua compra</h2>
-                    </div>
-                    <div class="panel-body">
-                        <img src="/mirrorfashion/img/produtos/foto1-<?= $_POST["cor"] ?>.png"
-                            class="img-thumbnail img-responsive">
-                        <dl>
-                            <dt>Cor</dt>
-                            <dd><?= $_POST['cor'] ?></dd>
-
-                            <dt>Tamanho</dt>
-                            <dd><?= $_POST['tamanho'] ?></dd>
-
-                            <dt>Produto</dt>
-                            <dd><?= $_POST['nome'] ?></dd>
-
-                            <dt>Preço</dt>
-                            <dd><?= $_POST['preco'] ?></dd>
-                        </dl>
-                    </div>
-                </div>
-
-                <form>
-                    <fieldset>
-                        <legend>Dados Pessoais</legend>
-
-                        <div class="form-group">
-                            <label for="nome">Nome completo</label>
-                            <input type="text" class="form-control" name="nome" id="nome" autofocus require>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email"
-                                placeholder="email@exemplo.com">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="cpf">CPF</label>
-                            <input type="text" class="form-control" id="cpf" name="cpf"
-                                placeholder="000.000.000-00" required>
-                        </div>
-
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value="sim" name="spam" checked>
-                                Quero receber spam da Mirror Fashion
-                            </label>
-                        </div>
-                    </fieldset>
-
-                    <fieldset>
-                        <legend>Cartão de Crédito</legend>
-
-                        <div class="form-group">
-                            <label for="numero-cartao">Número - CVV</label>
-                            <input type="text" class="form-control"
-                                    id="numero-cartao" name="numero-cartao">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="bandeira-cartao">bandeira</label>
-                            <select name="bandeira-cartao" id="bandeira-cartao"
-                                class="form-control">
-                                <option value="master">MasterCard</option>
-                                <option value="visa">Visa</option>
-                                <option value="amex">American Express</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="validade-cartao">Validade</label>
-                            <input type="month" class="form-control" 
-                                name="validade-cartao" id="validade-cartao">
-                        </div>
-
-                    </fieldset>
-
-                    <button type="submit" class="btn btn-primary btn-lg pull-right">
-                        <span class="glyphicon glyphicon-thumbs-up"></span>
-                        Confirmar Pedido
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">Mirror Fashion</a>
+                    <button class="navbar-toggle" type="button"
+                            data-target=".navbar-collapse" data-toggle="collapse">
+                        <Menu>
+                            <span class="glyphicon glyphicon-align-justify"></span>
+                        </Menu>
                     </button>
-                </form>
+                </div>
+                <ul class="nav navbar-nav nav navbar-nav collapse navbar-collapse">
+                    <li><a href="sobre.php">Sobre</a></li>
+                    <li><a href="#">Ajuda</a></li>
+                    <li><a href="#">Perguntas frequentes</a></li>
+                    <li><a href="#">Entre em contato</a></li>
+                </ul>
+            </nav>
+
+            <div class="jumbotron">
+                <div class="container">
+                    
+                    <h1>Ótima escolha!</h1>
+                    <p>Obrigado por comprar na Mirror Fashion!
+                    Preencha seus dados para efetivar a compra.
+                    </p>
+
+                    <div class="col-sm-4">
+                        <div class="panel panel-success">
+                            <div class="panel-heading">
+                                <h2 class="panel-title">Sua compra</h2>
+                            </div>
+                            <div class="panel-body">
+                                <img src="/mirrorfashion/img/produtos/foto1-<?= $_POST["cor"] ?>.png"
+                                    class="img-thumbnail img-responsive">
+                                <dl>
+                                    <dt>Cor</dt>
+                                    <dd><?= $_POST['cor'] ?></dd>
+
+                                    <dt>Tamanho</dt>
+                                    <dd><?= $_POST['tamanho'] ?></dd>
+
+                                    <dt>Produto</dt>
+                                    <dd><?= $_POST['nome'] ?></dd>
+
+                                    <dt>Preço</dt>
+                                    <dd><?= $_POST['preco'] ?></dd>
+                                </dl>
+                            </div>
+                        </div>
+                    </div>
+                    <form class="col-sm-8">
+                        <div class="row">
+                            <fieldset class="col-md-6">
+                                <legend>Dados Pessoais</legend>
+
+                                <div class="form-group">
+                                    <label for="nome">Nome completo</label>
+                                    <input type="text" class="form-control" name="nome" id="nome" autofocus require>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email"
+                                        placeholder="email@exemplo.com">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="cpf">CPF</label>
+                                    <input type="text" class="form-control" id="cpf" name="cpf"
+                                        placeholder="000.000.000-00" required>
+                                </div>
+
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" value="sim" name="spam" checked>
+                                        Quero receber spam da Mirror Fashion
+                                    </label>
+                                </div>
+                            </fieldset>
+
+                            <fieldset class="col-md-6">
+                                <legend>Cartão de Crédito</legend>
+
+                                <div class="form-group">
+                                    <label for="numero-cartao">Número - CVV</label>
+                                    <input type="text" class="form-control"
+                                            id="numero-cartao" name="numero-cartao">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="bandeira-cartao">bandeira</label>
+                                    <select name="bandeira-cartao" id="bandeira-cartao"
+                                        class="form-control">
+                                        <option value="master">MasterCard</option>
+                                        <option value="visa">Visa</option>
+                                        <option value="amex">American Express</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="validade-cartao">Validade</label>
+                                    <input type="month" class="form-control" 
+                                        name="validade-cartao" id="validade-cartao">
+                                </div>
+
+                            </fieldset>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-lg pull-right">
+                            <span class="glyphicon glyphicon-thumbs-up"></span>
+                            Confirmar Pedido
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
 </body>
 </html>
